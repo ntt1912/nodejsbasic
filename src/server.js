c8686771970088c5702d8e9b192b17c5468cb250
ntt1912 <124802515+ntt1912@users.noreply.github.com>
2023-05-08 20:35:26 +0700

    day7

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+configViewEngine(app);
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+app.get('/about', (req, res) => {
+    res.send('I am Thanh')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port: ${port}`)
+})
