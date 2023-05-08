27fdc9ef721f1c1f4a215ba7a0e60e6021fa60b1
ntt1912 <124802515+ntt1912@users.noreply.github.com>
2023-05-08 14:41:43 +0700

    new update

--- sever.js
+++ sever.js
@@ -10,5 +10,5 @@ app.get('/about', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port: ${port}`)
 })
